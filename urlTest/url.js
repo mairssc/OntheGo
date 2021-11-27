fcaf4c0b05d059a950e37c44ed1d1e2f67e7fde0
Sean Mairs <secmairs@gmail.com>
2021-11-27 12:05:28 -0800

    url.js creates unique url based on date, can also add additional random chars for safety

--- urlTest/url.js
+++ urlTest/url.js
@@ -2,15 +2,19 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-//Creates randomUrl, where url is the initial part of the url that is unchanged
+//Creates randomUrl based on date for the first 13 additional chars, then after can add additional chars
 function randomUrl(url, length) {
-    let charArray = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('');
-    for (var i = 0; i < length; i++) {
-        url += charArray[getRandomInt(charArray.length - 1)];
+    url += (new Date()).getTime();
+    if (length > 13) {
+        let charArray = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('');
+        length -= 13;
+        for (var i = 0; i < length; i++) {
+            url += charArray[getRandomInt(charArray.length - 1)];
+        }
     }
     return url;
 }
 
 for (let i = 0; i < 10; i++) {
-    console.log(randomUrl("http://localhost:8080/", 10))
+    console.log(randomUrl("http://localhost:8080/", 26))
 }
