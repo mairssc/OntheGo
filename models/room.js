25b42c327d77fb4407af03ca5137218aa308dcd0
Sean Mairs <secmairs@gmail.com>
2021-11-27 01:35:24 -0800

    added room model

---
+++ models/room.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const calendarSchema = require('../models/calendar.js');
+const url = 'mongodb://127.0.0.1:27017/node-mongo-hw' // change this as needed
+
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    url: {
+        type: String,
+        required: true
+    },
+    calendar: {
+        summary: {
+            type: String,
+            required: false
+        },
+        location: {
+            type: String,
+            required: false
+        },
+        start: {
+            dateTime: {
+                type: String,
+                required: true
+            },
+            timeZone: {
+                type: String,
+                required: true
+            },
+        },
+        end: {
+            dateTime: {
+                type: String,
+                required: true
+            },
+            timeZone: {
+                type: String,
+                required: true
+            },
+        },
+        recurrence: {
+            type: Array,
+            required: false
+        },
+        attendees: {
+            type: Array,
+            required: false
+        },
+        reminders: {
+            useDefault: {
+                type: Boolean,
+                required: false
+            },
+            overrides: {
+                type: Array,
+                required: false
+            }
+        }
+    },
+    // change depending on how users is implemented
+    users: {
+        type: [String],
+        required: true
+    }
+    // depends on how we do purchases
+    // purchases: {
+    //     type: [Number],
+    //     required: true
+    // }
+})
+
+module.exports = mongoose.model('room', roomSchema);
